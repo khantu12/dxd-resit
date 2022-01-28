884a989095e4a8aaf759771642fbf8aaee9597e0
Dimitar Dimitrov <i402384@fhict.nl>
2022-01-28 22:50:12 +0200

    Critical design finished

--- src/pages/CriticalDesignPage.tsx
+++ src/pages/CriticalDesignPage.tsx
@@ -4,6 +4,7 @@ import {
   H2,
   Image,
   Main,
+  OrderedList,
   Paragraph,
   Title,
 } from '../Components/elements';
@@ -96,9 +97,84 @@ const CriticalDesign: React.FC<{}> = () => (
       very promising. Now, I had to connect this idea with the COVID-19 vaccine
       and how this concept affects the view on the vaccine and its effects.
     </Paragraph>
+    <Paragraph>
+      With this basic idea in mind I have now started thinking about how I can
+      connect this with the COVID-19 vaccine and anti-vaxxers. The first thought
+      that came into mind was to have these this concept as follows: Have the
+      syringe from the previous iteration and put inside cubes. When these cubes
+      get pushed through, spheres will pop out.This idea sounded good at first
+      but after some critical thinking I threw away this idea. It does not
+      really make sense. The idea of the design won’t be as clear as I would
+      want it to.
+    </Paragraph>
+    <Paragraph>
+      Some other ideas came into mind like having the cap of an Covid-19 vaccine
+      test tube have this kind of square “filter” and when the sphere goes
+      through it becomes a cube as it shows the covid-19 vaccine through an
+      anti-vaxxer view. Again, the idea was not clear.
+    </Paragraph>
+    <Image src="/images/sketch_2.jpg" />
+    <Paragraph>
+      Then I thought of having the Covid-19 vaccine facts enter the
+      anti-vaxxer’s head through the filter: “I believe what I want to believe
+      that has also been shaped by the opinion of other people and theorists.
+      Going through this filter the facts become interpretation or something
+      completely different. Then the anti-vaxxer spits out this altered
+      interpretation of the effects in their covid-19 vaccine test tube.
+    </Paragraph>
+    <Image src="/images/sketch_3.jpg" />
+    <Paragraph>
+      I really liked this idea. I shared it with family and friends to hear of
+      their opinion by asking them various questions like:
+    </Paragraph>
+    <OrderedList>
+      <li>What do you understand from this image?</li>
+      <li>What do you think the message this image tries to send is?</li>
+      <li>…</li>
+    </OrderedList>
+    <Paragraph>
+      I got mixed answers. Some did not understand what was happening in the
+      image at all. Others understood what the image is but not the message. So
+      I drew a more refined version of the image and tried to eliminate these
+      issues.
+    </Paragraph>
+    <Image src="/images/final_result_1.jpg" />
+    <Paragraph>
+      People now understood what was happening in the image a bit better. By
+      some description from me they managed to also grasp what the idea and the
+      meaning of this image is. I want to take out the oral description in order
+      to understand the design. So I made a cleaner version and also drew some
+      variations (with text on the person's forehead, and one with text around
+      the sphere stating what it represents)
+    </Paragraph>
+    <div className="flex gap-1">
+      <Image className="flex-1" src="/images/final_result_half.jpg" />
+      <Image className="flex-1" src="/images/final_result_quarter.jpg" />
+      <Image className="flex-1" src="/images/final_result_full.jpg" />
+    </div>
+    <Paragraph>
+      I personally like the second image the best but having these variations
+      give me possibilities to test out what others think. Again, I reached out
+      to family and friends to give their opinion for each of the versions on
+      which they thought looked the most clear as a message. I also reached out
+      to some friends that I have not shared any of this yet to collect some
+      data from people that have absolutely no idea what this is about.
+    </Paragraph>
+    <Paragraph>
+      All of them chose the 3rd option because the message is clear and almost
+      everybody understood what the message is from the image. Though some of
+      them thought that the ball is not going through the filter so this
+      information is blocked which also contributes to the same idea. A nice
+      coincidence I did not think of but it turned out great. I agree with the
+      users and that is what I have chosen as my result.
+    </Paragraph>
 
     <H2>Result</H2>
-    <Paragraph>a</Paragraph>
+    <Image src="/images/final_result_full.jpg" />
+    <div className="flex gap-1">
+      <Image className="flex-1" src="/images/final_tshirt_mockup.png" />
+      <Image className="flex-1" src="/images/final_tshirt_mockup2.png" />
+    </div>
 
     <H2>Reflection</H2>
     <Paragraph />
